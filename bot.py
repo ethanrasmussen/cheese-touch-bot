110c58f0863da361da049a81e9fcc077e7efe391
ethanrasmussen <itseraz@gmail.com>
2020-06-03 09:47:52 -0500

    fix variables

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-import discord
 from discord.ext import commands
+import discord
 from token import token
 
 # discord bot token
@@ -17,29 +17,39 @@ async def on_ready():
     # set status
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name='Cheese Touch'))
 
-# TODO init cheese touch holder (random)
-cheesetouch_holder_id = ''
+# initialize CT holder w/ 'None' value
+cheesetouch_holder: discord.User = None
+
+# initialize dictionary of users crossing fingers
+# FORMAT: discord.User : Timestamp of last time fingers crossed (unix style)
 
 # poke command
+# TODO: account for 2 scenarios: pokee has fingers crossed; and pokee not in server
 @bot.command(name='poke', aliases=['touch'])
-async def await_poke(message, ctx, arg):
+async def await_poke(message, ctx, arg: discord.User):
     # check if message sender is the current cheesetouch holder
-    if str(message.author.id) == cheesetouch_holder_id:
+    if str(message.author.id) == str(cheesetouch_holder.id):
         arg = arg.replace("<", "")
         arg = arg.replace(">", "")
         arg = arg.replace("@", "")
-        cheesetouch_holder_id = str(arg)
-        cheesetouch_user = discord.User(id=int(cheesetouch_holder_id))
-        ctx.send(f"{message.author} passed the cheese-touch to {cheesetouch_user}!")
+        cheesetouch_holder = str(arg.id)
+        ctx.send(f"{message.author} passed the cheese-touch to {cheesetouch_holder}!")
     else:
         ctx.send(f"Hey {message.author}! You can't do that! You don't have the cheese touch!")
 
 # whois command
 @bot.command(name='whois', aliases=['who is'])
 async def await_whois(ctx):
-    cheesetouch_user = discord.User(id=int(cheesetouch_holder_id))
-    ctx.send(f"Currently, {cheesetouch_user}")
+    ctx.send(f"Currently, {cheesetouch_holder} has the cheese touch!")
 
 # TODO cross command
 
+# TODO init command
+
 # TODO help command
+
+
+
+
+# run the bot
+bot.run(TOKEN.strip())
