50858003512d8f3f9dbaa15850098343816e5fcd
ethanrasmussen <itseraz@gmail.com>
2020-06-02 20:10:05 -0500

    poke cmd

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from token import token
 # discord bot token
 TOKEN = token
 
+# init bot
 bot = commands.Bot(command_prefix=('cheesetouch', 'cheese', 'Cheese', 'Cheesetouch', 'ct', 'CT'), case_insensitive=True, help_command=None)
 
 # when bot online
@@ -16,4 +17,24 @@ async def on_ready():
     # set status
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name='Cheese Touch'))
 
+# TODO init cheese touch holder (random)
+cheesetouch_holder_id = ''
+
 # poke command
+@bot.command(name='poke', aliases=['touch'])
+async def await_poke(message, ctx, arg):
+    # check if message sender is the current cheesetouch holder
+    if str(message.author.id) == cheesetouch_holder_id:
+        arg = arg.replace("<", "")
+        arg = arg.replace(">", "")
+        arg = arg.replace("@", "")
+        cheesetouch_holder_id = str(arg)
+        ctx.send(f"{message.author} passed the cheese-touch to {cheesetouch_holder_id}!")
+    else:
+        ctx.send(f"Hey {message.author}! You can't do that! You don't have the cheese touch!")
+
+# TODO whois command
+
+# TODO cross command
+
+# TODO help command
